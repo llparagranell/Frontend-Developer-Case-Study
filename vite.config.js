485832ccf1f7281a8c3917390fc13a9ca63d947b
llparagranell <paragrane000@gmail.com>
2025-05-25 11:48:02 +0530

    final commits

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/Frontend-Developer-Case-Study/', // 👈 very important
   plugins: [react()],
 })
